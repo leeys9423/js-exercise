75e51755a74798d42e76809c2cbad5d0c9f3cbbc
yslee <leeys9423@gmail.com>
2024-05-15 12:12:01 +0900

    ✨ 단락 평가

---
+++ chapter02.js
@@ -0,0 +1,20 @@
+function returnFalse() {
+  console.log("False 함수");
+  return false;
+}
+
+function returnTrue() {
+  console.log("True 함수");
+  return true;
+}
+
+console.log(returnTrue() && returnFalse());
+
+// 단락 평가 활용 사례
+function printName(person) {
+  const name = person && person.name;
+  console.log(name || "person의 값이 없음");
+}
+
+printName();
+printName({ name: "이영섭" });
