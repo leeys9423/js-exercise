06704e45ca13c4959a66764b66a7928b547b56d8
yslee <leeys9423@gmail.com>
2024-05-18 23:24:12 +0900

    ✨ 비동기 작업 처리하기 2. Promise

---
+++ chapter13.js
@@ -0,0 +1,55 @@
+// then 메서드
+// -> 그 후에
+// 성공했을때만 호출
+// promise.then((value) => {
+//   console.log(value);
+// });
+
+// catch 메서드
+// 실패했을때만 호출
+// promise.catch((error) => {
+//   console.log(error);
+// });
+
+// 보통은 한꺼번에 사용
+// 체이닝
+// promise
+//   .then((value) => {
+//     console.log(value);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+function add10(num) {
+  const promise = new Promise((resolve, reject) => {
+    // 비동기 작업 실행하는 함수
+    // executor
+
+    setTimeout(() => {
+      if (typeof num === "number") {
+        resolve(num + 10);
+      } else {
+        reject("num이 숫자가 아닙니다.");
+      }
+    }, 2000);
+  });
+
+  return promise;
+}
+
+add10(0)
+  .then((result) => {
+    console.log(result);
+    return add10(result);
+  })
+  .then((result) => {
+    console.log(result);
+    return add10(undefined);
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
